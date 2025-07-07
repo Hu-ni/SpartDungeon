137bef200cc516e591a779c0b8e37d49508bba38
ParkHun <hundl5789@gmail.com>
2025-07-07 10:57:02 +0900

    Initial commit

---
+++ Spartdungeon/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace SpartDungeon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            return;
+        }
+    }
+}
---
+++ Spartdungeon/User/UserStatic.cs
@@ -0,0 +1,22 @@
+﻿using Spartdungeon.User.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spartdungeon.User
+{
+    public static class UserStatic
+    {
+        public const int LEVEL = 1;
+        public const int HEALTH = 150;
+        public const int ATTACK = 10;
+        public const int DEFENSE = 5;
+
+        public Status GetDefaultStatus()
+        {
+            return new Status (){ HEALTH, ATTACK, DEFENSE };
+        }
+    }
+}
